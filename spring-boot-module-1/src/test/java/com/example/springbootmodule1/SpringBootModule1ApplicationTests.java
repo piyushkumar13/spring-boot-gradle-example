ce03a6d6174d1d666547a80e0a7a08adc6506d21
piyushkumar13 <piyush.k9013@gmail.com>
2024-01-27 23:19:55 +0530

    initial commit

---
+++ spring-boot-module-1/src/test/java/com/example/springbootmodule1/SpringBootModule1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmodule1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootModule1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-boot-module-2/src/main/java/com/example/springbootmodule2/SpringBootModule2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootmodule2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModule2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootModule2Application.class, args);
+    }
+
+}
